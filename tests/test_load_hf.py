af173b7615a22f1ac7bb997c0d727c5f896d265a
Victor Morand <morand@isir.upmc.fr>
2026-04-09 11:38:21 +0200

    fix: removed submodule - xpm-torch added as git dependency

--- tests/test_load_hf.py
+++ tests/test_load_hf.py
@@ -1,31 +1,22 @@
-import llm2ner
 from xpm_torch.huggingface import TorchHFHub
+from llm2ner import ToMMeR, utils
 
-
-# Load the model using the loader (which can be used for more complex loading scenarios)
-tommer_loader = TorchHFHub.pretrained_loader("updated_model_local")
-print("loaded config:")
-print(tommer_loader)
-
-# Or load as a ready-to-use instance for direct inference
-tommer = TorchHFHub.from_pretrained("updated_model_local")
-
-print("loaded model:")
-print(tommer)
+tommer: ToMMeR = TorchHFHub.from_pretrained("llm2ner/ToMMeR-Llama-3.2-1B_L6_R64")
 # load Backbone llm, optionnally cut the unused layer to save GPU space.
-llm = llm2ner.utils.load_llm(
-    tommer.llm_name,
-    cut_to_layer=tommer.layer,
-)
+llm = utils.load_llm( tommer.llm_name, cut_to_layer=tommer.layer,)
 tommer.to(llm.device)
 
-text = "Large language models are awesome. While trained on language modeling, they exhibit emergent Zero Shot abilities that make them suitable for a wide range of tasks, including Named Entity Recognition (NER). "
+#### Raw Inference
+text = ["Large language models are awesome"]
+print(f"Input text: {text[0]}")
 
-
-# tokenize in shape (1, seq_len)
+#tokenize in shape (1, seq_len)
 tokens = llm.tokenizer(text, return_tensors="pt")["input_ids"].to(llm.device)
+# Output raw scores
+output = tommer.forward(tokens, llm) # (batch_size, seq_len, seq_len)
+print(f"Raw Output shape: {output.shape}")
 
-# use given decoding strategy to infer entities
-entities = tommer.infer_entities(tokens=tokens, model=llm, threshold=0.5)
-str_entities = [llm.tokenizer.decode(tokens[0, b : e + 1]) for b, e in entities[0]]
+#use given decoding strategy to infer entities
+entities = tommer.infer_entities(tokens=tokens, model=llm, threshold=0.5, decoding_strategy="greedy")
+str_entities = [ llm.tokenizer.decode(tokens[0,b:e+1]) for b, e in entities[0]]
 print(f"Predicted entities: {str_entities}")
